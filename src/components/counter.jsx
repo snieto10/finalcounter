ba6cea85c3b1a9f57e85a839d14adbed7cc77561
Sergio Nieto <snieto10@gmail.com>
2022-09-30 22:38:31 -0400

    add increment

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,11 +6,18 @@ class Counter extends Component {
     count: 0,
   };
 
+  handleAdd = () => {
+    let count = this.state.count + 1;
+    this.setState({ count });
+  };
+
   render() {
     return (
       <React.Fragment>
         <div className="counter-block yellow">{this.state.count}</div>
-        <div className="counter-block green">+</div>
+        <div className="counter-block green" onClick={this.handleAdd}>
+          +
+        </div>
         <div className="counter-block red">-</div>
         <div className="counter-block">
           <button className="blue">Delete</button>
