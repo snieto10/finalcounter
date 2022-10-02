c68947db4a5923ac82f632d9fcfe1f8f96908c48
Sergio Nieto <snieto10@gmail.com>
2022-10-02 17:25:13 -0400

    input field is working

--- src/components/toDoList.jsx
+++ src/components/toDoList.jsx
@@ -1,22 +1,39 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import "./toDoList.css";
 
 class ToDoList extends Component {
+  state = {
+    account: { username: "", password: "" },
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submitted");
+  };
+
+  handleChange = (e) => {
+    const account = { ...this.state.account };
+    account.username = e.currentTarget.value;
+    this.setState({ account });
+    console.log(account);
+  };
+
   render() {
     return (
       <div>
-        <h1>To Do List</h1>
-        <ul>
-          <li>
-            <Link to="/todolist/house">HOUSE</Link>
-          </li>
-          <li>
-            <Link to="/todolist/car">CAR</Link>
-          </li>
-          <li>
-            <Link to="/todolist/boat">BOAT</Link>
-          </li>
-        </ul>
+        <h1 className="title">To Do List</h1>
+        <form onSubmit={this.handleSubmit} className="form">
+          <input
+            value={this.state.account.username}
+            onChange={this.handleChange}
+            className="inputsize"
+            type="text"
+            placeholder="Enter Task"
+            id="data"
+          />
+          <button className="btn-submit">ADD</button>
+        </form>
       </div>
     );
   }
